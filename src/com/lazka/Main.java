b1fecd4d9d029d62d63f6878afde3a7d991c4116
Lazareva <YLazareva@kiev.luxoft.com>
2018-07-09 21:06:16 +0300

    Initial commit

---
+++ src/com/lazka/Main.java
@@ -0,0 +1,14 @@
+package com.lazka;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        reader.readLine();
+    }
+}
+
+
